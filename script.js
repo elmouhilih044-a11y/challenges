1186ab94b6b60f7cd1f005180b9b05792efe5295
elmouhilih044-a11y <elmouhili.h044@gmail.com>
2025-11-21 10:27:11 +0100

    exercice 1

--- script.js
+++ script.js
@@ -1,67 +1,86 @@
+
+// // Ex1:
+// function getFirstEmployeeFirstName(employees) {
+//     return employees[0].firstName;
+// }
+// console.log(getFirstEmployeeFirstName(employees))
+
+
+// // Ex2:
+
+// function getLastEmployeeLastName(employees) {
+//     return employees[employees.length-1].firstName;
+// }
+// console.log(getLastEmployeeLastName(employees))
+
+// // Ex3:
+
+// function getEmployeeDepartmentById(employees,ID){
+//     for(let i=0;i<employees.length-1;i++){
+//         if(ID===employees[i].id)
+//             return employees[i].department
+//     }
+// }
+// console.log(getEmployeeDepartmentById(employees,5))
+
+// // Ex5:
+// function hasInactiveEmployees(employees){
+//       for(let i=0;i<employees.length-1;i++){
+//         if(employees[i].isActive===false)
+//             return true;
+//     }
+// }
+// console.log(hasInactiveEmployees(employees))
+
+
+// // Ex8
+
+// function countEmployeesInDepartment(employees,department){
+//     let count=0;
+//     for(let i=0;i<employees.length-1;i++){
+//         if(employees[i].department===department){
+//             count++;
+//         }
+//     }
+//       return count
+// }
+// console.log(countEmployeesInDepartment(employees,"Développement"))
+
+
+// // Ex9
+
+// function getUniqueDepartments(employees) {
+//   const departments = [];
+
+//   for (let i = 0; i < employees.length-1; i++) {
+//     let department = employees[i].department;
+//     let x= false;
+
+//     for (let z = 0; z < departments.length; z++) {
+//       if (departments[z] === department) {
+//         x= true;
+//         break;
+//       }
+//     }
+// }
+// }
+// console.log(getUniqueDepartments(employees));
+
 import { employees } from "./dataset.js";
-// Ex1:
-function getFirstEmployeeFirstName(employees) {
-    return employees[0].firstName;
+
+const employeeTd = document.getElementById("employees-tbody");
+const container = document.getElementById("employees-container");
+
+function afficherEmployes(employees) {
+  employees.forEach((employee) => {
+    employeeTd.innerHTML = `
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.department}</td>
+    <td>${employee.position}</td>
+    <td>${employee.salary}</td>
+    `
+  })
+
 }
-console.log(getFirstEmployeeFirstName(employees))
-
-
-// Ex2:
-
-function getLastEmployeeLastName(employees) {
-    return employees[employees.length-1].firstName;
-}
-console.log(getLastEmployeeLastName(employees))
-
-// Ex3:
-
-function getEmployeeDepartmentById(employees,ID){
-    for(let i=0;i<employees.length-1;i++){
-        if(ID===employees[i].id)
-            return employees[i].department
-    }
-}
-console.log(getEmployeeDepartmentById(employees,5))
-
-// Ex5:
-function hasInactiveEmployees(employees){
-      for(let i=0;i<employees.length-1;i++){
-        if(employees[i].isActive===false)
-            return true;
-    }
-}
-console.log(hasInactiveEmployees(employees))
-
-
-// Ex8
-
-function countEmployeesInDepartment(employees,department){
-    let count=0;
-    for(let i=0;i<employees.length-1;i++){
-        if(employees[i].department===department){
-            count++;
-        }
-    }
-      return count
-}
-console.log(countEmployeesInDepartment(employees,"Développement"))
-
-
-// Ex9
-
-function getUniqueDepartments(employees) {
-  const departments = [];
-
-  for (let i = 0; i < employees.length-1; i++) {
-    let department = employees[i].department;
-    let x= false;
-
-    for (let z = 0; z < departments.length; z++) {
-      if (departments[z] === department) {
-        x= true;
-        break;
-      }
-    }
-}
-}
-console.log(getUniqueDepartments(employees));
+afficherEmployes(employees);
