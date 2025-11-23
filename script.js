c10b15a89a02411d3ebfd4a2043da1ff69a92930
elmouhilih044-a11y <elmouhili.h044@gmail.com>
2025-11-23 02:24:09 +0100

    Exercice 4

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
+import { employees } from "./data.js"
 
-btn.addEventListener('click',(e)=>{
-    e.preventDefault()
-    const email=document.querySelector(".email").value.trim()
-    
-    function validerEmail() {
-        
-        const regex=/^[\w.-\s]+@[\w.-]+\.\w{2,}$/
-        if(email===""){
-            alert("le champ email est vide")
-        }
-        else if(!regex.test(email)){
-            alert("entrez format correct d'email")
-        }
-        else{
-            alert("email valide")
-        }
+// function 1
+function getEmployeesByDepartment(department) {
+   const Result=employees.filter(employee=>employee.department===department)
+    console.log(Result)
+}
+getEmployeesByDepartment("Marketing") 
+
+// function 2
+function augmenterSalaires(pourcentage) {
+   const Result=employees.map(employee=>employee.salary+(pourcentage*employee.salary)/100)
+   console.log(Result)
+    // Augmente tous les salaires du pourcentage donné
+    // Retourne le nouveau tableau
+}
+augmenterSalaires(20)
+
+// function 3
+function getDepartmentStats() {
+    let count=0;
+    let som=0;
+    for(let employee of employees){
+        count++
     }
-    const name=document.querySelector(".name").value.trim()
-    function validerNom() {
-        const regex=/^[A-Za-z\s]+$/
-        if(name===""){
-            alert("le champ name est vide")
-        }
-        else if(!regex.test(name)){
-            alert("entrez format correct de nom")
-        }
-        else{
-            alert("nom valide")
-        }
+    for(let employee of employees){
+       som=som+employee.salary;
     }
-    const tele=document.querySelector(".telephone").value.trim()
-    function validerTelephone() {
-     const regex=/^\+33\d{9}$/
-        if(tele===""){
-            alert("le champ telephone est vide")
-        }
-        else if(!regex.test(tele)){
-            alert("entrez format correct de num telephone")
-        }
-        else{
-            alert("num telephone valide")
-        }
+    const salaryMoyen=som/count
+ let max=employees[0].salary
+  for(let employee of employees){
+  if(employee.salary>max){
+    max=employee.salary
+  }
+
+    }
+    // Retourne un objet avec pour chaque département :
+    // - nombre d'employés
+    // - salaire moyen
+    // - salaire maximum
+    console.log(count)
+    console.log(salaryMoyen)
+    console.log(max)
 }
 
+getDepartmentStats() 
 
-    validerEmail()
-    validerNom()
-    validerTelephone()
-})
-
+// function 4
+function getEmployeesWithSkill(skill){
+   return employees.filter(employee=>employee.skills.includes(skill))
+}
+console.log(getEmployeesWithSkill("JavaScript"))
